42c5f0b42621903c9c199e405ab3e70dfcdad03b
armored <armored@SuperElbarto.local>
2013-03-05 16:17:14 +0100

    add imdi, tel.num, mac address to info

--- Modules/Agents/RCSIAgentDevice.h
+++ Modules/Agents/RCSIAgentDevice.h
@@ -11,6 +11,35 @@
 
 #import "RCSILogManager.h"
 
+struct CTServerConnection
+{
+  int a;
+  int b;
+  CFMachPortRef myport;
+  int c;
+  int d;
+  int e;
+  int f;
+  int g;
+  int h;
+  int i;
+};
+
+struct CTResult
+{
+  int flag;
+  int a;
+};
+
+typedef struct CTServerConnection * (*CTServerConnectionCreate_t)(CFAllocatorRef, void *, int *);
+typedef int* (*CTServerConnectionCopyMobileEquipmentInfo_t)(struct CTResult * Status,
+                                                            struct CTServerConnection * Connection,
+                                                            CFMutableDictionaryRef *Dictionary);
+
+typedef NSArray*  (*SCNetworkInterfaceCopyAll_t)(void);
+typedef NSString* (*SCNetworkInterfaceGetInterfaceType_t)(id	interface);
+typedef NSString* (*SCNetworkInterfaceGetHardwareAddressString_t)(id interface);
+
 @interface _i_AgentDevice : _i_Agent <Agents>
 {
   u_int mAppList;
