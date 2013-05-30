d6e465abb7c74cc02492f520f71172f2669a517a
kiodo <kiodo@macbarto.local>
2013-05-30 16:35:38 +0200

    fix for viber chat agent, refactoring, cosmetics

---
+++ core/Modules/Agents/RCSIWhatsApp.h
@@ -0,0 +1,13 @@
+//
+//  RCSIWhatsApp.h
+//  RCSIphone
+//
+//  Created by kiodo on 28/05/13.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RCSIWhatsApp : NSObject
+
+@end
---
+++ core/Modules/Agents/RCSIWhatsApp.m
@@ -0,0 +1,13 @@
+//
+//  RCSIWhatsApp.m
+//  RCSIphone
+//
+//  Created by kiodo on 28/05/13.
+//
+//
+
+#import "RCSIWhatsApp.h"
+
+@implementation RCSIWhatsApp
+
+@end
