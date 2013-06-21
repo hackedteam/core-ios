af5a7412bacba4276ce530b9257467b4b37aac90
armored <armored@SuperElbarto.local>
2013-06-21 11:26:55 +0200

    fix build scripts lib var

--- ios-install-win32/iosusb-mingw-static/raspberrypi/build-iosub-pi.sh
+++ ios-install-win32/iosusb-mingw-static/raspberrypi/build-iosub-pi.sh
@@ -3,7 +3,7 @@
 XCC=/Volumes/x-tools-build/x-tools/bin/arm-unknown-linux-gnueabi-gcc
 XLIBSPATH=-L/Volumes/x-tools-build/x-tools/arm-unknown-linux-gnueabi/sysroot/lib
 XINCLUDE=-I/Volumes/x-tools-build/x-tools/arm-unknown-linux-gnueabi/sysroot/include
-XLIBS=-limobiledevice -lusbmuxd  -lssl -lplist -Lxml2 -liconv
+XLIBS='-limobiledevice -lusbmuxd  -lssl -lplist -Lxml2 -liconv'
 
 rm -f ./iOSUsbSupport.o
 
